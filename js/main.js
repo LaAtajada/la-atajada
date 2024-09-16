057ee7c2c73f6db18dbc1dad33de57fd92e5bb9c
Horacio Caniz <horacio.caniz@galileo.edu>
2024-09-16 13:32:34 -0600

    Cambios a los footers y al main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('La ATAJADA está lista');
 
-    // Seleccionamos el ícono del menú y la lista de enlaces
-const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.getElementById('nav-links');
-
-// Añadimos un evento de clic al ícono del menú
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+    
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('show'); // Alterna la clase "show" para mostrar/ocultar el menú
+    });
+    
 
 });
